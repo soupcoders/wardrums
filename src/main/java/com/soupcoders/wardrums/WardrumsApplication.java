d80aa7eaf2fa657ed1985ae32fc9c1ed29e63c85
TB <soupcoders@gmail.com>
2019-06-21 16:11:23 +0200

    Initial commit

---
+++ src/main/java/com/soupcoders/wardrums/WardrumsApplication.java
@@ -0,0 +1,13 @@
+package com.soupcoders.wardrums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WardrumsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WardrumsApplication.class, args);
+    }
+
+}
